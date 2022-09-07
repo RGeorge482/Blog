4a5dfc3a7a3b194d3c4dac9d96d0670dcb86e769
RGeorge482 <ratagheorghita@yahoo.com>
2022-09-07 15:48:47 +0000

    App.js folder paths

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,10 +1,23 @@
 import TopBar from "./components/topbar/TopBar";
+import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <TopBar/>
+      <Switch>
+      <Route exact path="/">
+          <Home />
+        </Route>
+      <Route path="/posts">
+          <Home />
+        </Route>
+      <Route path="/register">
+          {currentUser ? <Home /> : <Register />}
+        </Route>  
+      </Switch>
     </Router>
   );
 }
