feb40b9557cc4406ac24d3b4ee6ede2c3473db9b
RGeorge482 <ratagheorghita@yahoo.com>
2022-09-15 18:07:47 +0000

    header

---
+++ blog/src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css'
+import header from '../../assets/header.png';
+
+export default function Header() {
+    return (
+      <div className='header'>
+        
+        </div>
+    )
+  }
