0876a2c691fea5371ca6ae7d864a67ed028892e3
RGeorge482 <ratagheorghita@yahoo.com>
2022-09-19 12:53:34 +0000

    Header

--- blog/src/components/header/Header.jsx
+++ blog/src/components/header/Header.jsx
@@ -4,7 +4,11 @@ import header from '../../assets/header.png';
 export default function Header() {
     return (
       <div className='header'>
-        
-        </div>
+        <div className="headerTitles">
+        <span className='headerTitleSm'>Computing & IT</span>
+        <span className='headerTitleLg'>Blog</span>
+      </div>
+      <img className='headerImg' src={header} alt="header foto" />  
+      </div>
     )
   }
