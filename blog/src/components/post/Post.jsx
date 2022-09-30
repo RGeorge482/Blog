e219503405650f65e7aff5c465fd4c72232767c6
RGeorge482 <ratagheorghita@yahoo.com>
2022-09-30 14:33:48 +0000

    Post

--- blog/src/components/post/Post.jsx
+++ blog/src/components/post/Post.jsx
@@ -5,7 +5,12 @@ export default function Post() {
     return (
       <div className="post">
         <img className="postImg" src={postImg} alt="post img" />
-         
+          <div className="postInfo">
+          <div className="postCats">
+            <span className="postCat">HTML</span>
+          </div>
+          <span className="posttitle">Lorem ipsum dolorel fsit amet</span>
+        </div>
         </div>
     )
   }
