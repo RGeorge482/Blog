03017f39decaf2a489f8a9c5c587c14f81354829
RGeorge482 <ratagheorghita@yahoo.com>
2022-10-30 18:38:19 +0000

    sideBar categories

--- blog/src/components/sideBar/SideBar.jsx
+++ blog/src/components/sideBar/SideBar.jsx
@@ -9,6 +9,19 @@ export default function SideBar() {
                 <img className='aboutMeImg' src={aboutMe} alt="about me" />
                 <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque, laborum exercitationem. Possimus officia quo mollitia? Culpa maiores quaerat repellendus.</p>
             </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">Networking</li>
+                    <li className="sidebarListItem">HTML</li>
+                    <li className="sidebarListItem">CSS</li>
+                    <li className="sidebarListItem">Java Script</li>
+                    <li className="sidebarListItem">React</li>
+                    <li className="sidebarListItem">Java</li>
+                    <li className="sidebarListItem">Python</li>
+                    <li className="sidebarListItem">Operating System</li>
+                </ul>
+            </div>
         </div>
     )
 }
