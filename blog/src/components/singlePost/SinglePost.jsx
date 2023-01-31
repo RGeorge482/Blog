81aa59f4832a0539822d8af5f189e53df126e9bd
RGeorge482 <ratagheorghita@yahoo.com>
2023-01-31 15:00:29 +0000

    Single post

--- blog/src/components/singlePost/SinglePost.jsx
+++ blog/src/components/singlePost/SinglePost.jsx
@@ -7,6 +7,13 @@ export default function SinglePost() {
       <div className="singlePostWrapper">
         <img src={singlePostImg} alt="single post" className='singlePostImg' />
       </div>
+      <h1 className="singlePostTitle">
+        Lorem ipsum dolor sit amet
+        <div className="singlePostEdit">
+        <i className="singlePostIcon fa-solid fa-pen-to-square"></i>
+        <i className="singlePostIcon fa-solid fa-trash-can"></i>
+        </div>
+      </h1>
       </div>
   )
 }
