bc32a43aa5a94bafef1c3ae487fbd469b26cada2
RGeorge482 <ratagheorghita@yahoo.com>
2023-02-01 12:08:02 +0000

    Single Post

--- blog/src/components/singlePost/SinglePost.jsx
+++ blog/src/components/singlePost/SinglePost.jsx
@@ -14,6 +14,11 @@ export default function SinglePost() {
         <i className="singlePostIcon fa-solid fa-trash-can"></i>
         </div>
       </h1>
+      <div className="singlePostInfo">
+        <span className='singlePostAuthor'>Autor: <b>George</b></span>
+        <span className="singlePostDate">1 hour ago</span>
       </div>
+      <p className='singlePostDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nulla exercitationem rerum at? Id voluptas quidem fuga veniam quibusdam molestiae illo quia animi. Maiores omnis voluptas facere impedit aliquid reiciendis. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam laborum aperiam suscipit voluptate dolor ut hic atque id nemo laudantium quis cum sit veritatis perferendis, fugiat voluptates error natus blanditiis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat pariatur itaque veniam laborum ipsam, iste obcaecati facere quibusdam harum ipsa voluptate sit iure quam quasi ab perspiciatis totam unde odit!</p>
+    </div>
   )
 }
