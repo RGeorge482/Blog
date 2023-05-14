e5e5741b385b4d06555569a399daa41b1aeadc6f
RGeorge482 <ratagheorghita@yahoo.com>
2023-05-14 18:46:23 +0000

    Login

--- blog/src/pages/login/Login.jsx
+++ blog/src/pages/login/Login.jsx
@@ -8,7 +8,6 @@ export default function Login() {
       <form className="loginForm">
         <label>Email</label>
         <input className="loginInput" type="text" placeholder="Enter your email..." />   
-        
       </form>     
       </div>
   )
